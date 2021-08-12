d365b911e062dbaf0600143d32906cad755d4665
Antonis Christoforakis <ant.christoforakis@gmail.com>
2021-08-12 15:23:06 +0300

    initial commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://Antonis:california2@cluster0.1xp0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Inserted' });
+  }
+}
+
+export default handler;
